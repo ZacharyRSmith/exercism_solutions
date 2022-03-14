f7b43a6f992561ac52c05ded26e55fb2f538f7b2
Zachary Ryan Smith <zach@angaza.com>
2022-03-13 21:37:10 -0400

    refactor

--- users/saltheads/python/ledger/ledger.py
+++ users/saltheads/python/ledger/ledger.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 
+def _fmt_date(date, locale):
+    y, m, d = date
+    if locale == 'en_US':
+        return f'{m}/{d}/{y}'
+    elif locale == 'nl_NL':
+        return f'{d}-{m}-{y}'
+
+
+def _fmt_change(change, locale, currency):
+    if locale == 'en_US' and currency == 'USD':
+        absolute = f'${abs(change)/100:,.2f}'
+        money = f'({absolute})' if change < 0 else f' {absolute} '
+    elif locale == 'en_US' and currency == 'EUR':
+        absolute = f'€{abs(change)/100:,.2f}'
+        money = f'({absolute})' if change < 0 else f' {absolute} '
+    elif locale == 'nl_NL' and currency == 'USD':
+        money = f'$ {change/100:,.2f} '.translate(
+            str.maketrans({',': '.', '.': ','}))
+    elif locale == 'nl_NL' and currency == 'EUR':
+        absolute = f'€ {abs(change)/100:,.2f}'.translate(
+            str.maketrans({',': '.', '.': ','}))
+        money = f'({absolute})' if change < 0 else f' {absolute} '
+    return money
+
+
 def create_entry(date, description, change):
     return tuple([tuple(list(date.split('-'))), description, change])
 
 
 def format_entries(currency, locale, entries):
+    if locale not in ('en_US', 'nl_NL'):
+        raise ValueError("BOOM!")
     entries = sorted(entries)
     lines = []
     if locale == 'en_US':
@@ -14,29 +41,9 @@ def format_entries(currency, locale, entries):
     else:
         pass
     for entry in entries:
-        (y, m, d), description, change = entry
+        date, description, change = entry
         description = description \
             if len(description) <= 25 else description[:22] + '...'
-        if locale == 'en_US':
-            date = f'{m}/{d}/{y}'
-        elif locale == 'nl_NL':
-            date = f'{d}-{m}-{y}'
-        else:
-            pass
-        if locale == 'en_US' and currency == 'USD':
-            absolute = f'${abs(change)/100:,.2f}'
-            money = f'({absolute})' if change < 0 else f' {absolute} '
-        elif locale == 'en_US' and currency == 'EUR':
-            absolute = f'€{abs(change)/100:,.2f}'
-            money = f'({absolute})' if change < 0 else f' {absolute} '
-        elif locale == 'nl_NL' and currency == 'USD':
-            money = f'$ {change/100:,.2f} '.translate(
-                str.maketrans({',': '.', '.': ','}))
-        elif locale == 'nl_NL' and currency == 'EUR':
-            absolute = f'€ {abs(change)/100:,.2f}'.translate(
-                str.maketrans({',': '.', '.': ','}))
-            money = f'({absolute})' if change < 0 else f' {absolute} '
-        else:
-            pass
-        lines.append(f'{date: <10} | {description: <25} | {money: >13}')
+
+        lines.append(f'{_fmt_date(date, locale)} | {description: <25} | {_fmt_change(change, locale, currency): >13}')
     return '\n'.join(lines)
